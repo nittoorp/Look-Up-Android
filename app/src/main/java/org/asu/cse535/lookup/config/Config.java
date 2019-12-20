a51f685ca1487ec1d864c1b2cc5778dc96dcd7b1
nittoorp <nittoorp@gmail.com>
2019-12-20 01:45:02 -0700

    Update Config.java

--- app/src/main/java/org/asu/cse535/lookup/config/Config.java
+++ app/src/main/java/org/asu/cse535/lookup/config/Config.java
@@ -3,6 +3,6 @@ package org.asu.cse535.lookup.config;
 public class Config {
 
     public static final String PLACES_NEAR_ME_API_BASE_URL = "https://maps.googleapis.com";
-    public final static String PLACES_NEAR_ME__API_KEY = "AIzaSyDbhiX7Yo5hXQMxobqEZ5HGHs0QKYOfffU";
+    public final static String PLACES_NEAR_ME__API_KEY = "API KEY";
 
 }
